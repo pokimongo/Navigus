45529f7576ca717d4538f7d7b4ae480263729a40
pokimongo <33128078+pokimongo@users.noreply.github.com>
2020-07-12 03:14:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -76,4 +76,4 @@ def show_index():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True,threaded=True)
+    app.run(host='0.0.0.0', port=5000, debug=False,threaded=True)
